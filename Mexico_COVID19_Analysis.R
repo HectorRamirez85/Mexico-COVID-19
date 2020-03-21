bc7b92d88deb880f9c827eb8562c5c335cf973b1
HectorRamirez85 <40415662+HectorRamirez85@users.noreply.github.com>
2020-03-20 22:23:32 -0600

    Update Mexico_COVID19_Analysis.R

--- Mexico_COVID19_Analysis.R
+++ Mexico_COVID19_Analysis.R
@@ -1,3 +1,7 @@
+#### Libraries ####
+require(ggplot2)
+
+#### Directories #####
 setwd("G:\\Mi unidad\\Covid19\\Mexico-COVID-19")
 
 
@@ -6,15 +10,26 @@ table <- read.csv("Mexico_COVID19.csv")
 
 dim(table)
 str(table)
+head(table)
 
 
-table[nrow(table),]
+subtable <- table[-(1:35),] # removing the first 35 days
 
-grep("_L", names(table))
-grep("_I", names(table))
 
-plot(table$Pos, type="o", pch=19, las=1, ylab="Infectados",
-     main="Infectados México", xlab="tiempo (días)")
+# plot(table$Pos, type="o", pch=19, las=1, ylab="Infected",
+#      main="Infected", xlab="time (days)")
+
+ggplot() +
+  ylab("Infected people") + xlab("Date") + ggtitle("Mexico infected people") +
+  geom_line(subtable, mapping=aes(x=Fecha, y=Pos, group=1)) +
+  geom_point(subtable, mapping=aes(x=Fecha, y=Pos, group=1)) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  geom_hline(yintercept = 100, lty=2) +
+  geom_line(subtable, mapping=aes(x=Fecha, y=Deceased, group=1), col="red") +
+  geom_point(subtable, mapping=aes(x=Fecha, y=Deceased, group=1), col="red") +
+  scale_color_discrete(name = "Y series", labels = c("Deceased", "Pos"))
+
 
 # legend imported, locals, total, deads
 

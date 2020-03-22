c982a8fd2dbe01615e4069f301c28667ef9da325
HectorRamirez85 <40415662+HectorRamirez85@users.noreply.github.com>
2020-03-21 18:01:11 -0600

    Update Mexico_COVID19_Analysis.R

--- Mexico_COVID19_Analysis.R
+++ Mexico_COVID19_Analysis.R
@@ -13,29 +13,52 @@ str(table)
 head(table)
 
 
-subtable <- table[-(1:35),] # removing the first 35 days
+subtable <- table[-(1:35),] # removing the first 35 days without cases
 
 
 # plot(table$Pos, type="o", pch=19, las=1, ylab="Infected",
 #      main="Infected", xlab="time (days)")
 
+dim(subtable)
 
-pdf("InfectionEvolution.pdf",11,9)
-ggplot() +
+pdf("MexicoInfectionEvolution.pdf",11,9)
+for(i in 1:nrow(subtable)){
+  print(i)
+  ggplot(subtable[i,]) +
+    ylab("Infected people") + xlab("Date") + ggtitle("Mexico infected people") +
+    ylim(c(0,250)) +
+    geom_line(mapping=aes(x=Fecha, y=Pos, group=1)) +
+    geom_point(mapping=aes(x=Fecha, y=Pos, group=1)) +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    geom_hline(yintercept = 100, lty=2) +
+    geom_line(mapping=aes(x=Fecha, y=Deceased, group=1), col="red") +
+    geom_point(mapping=aes(x=Fecha, y=Deceased, group=1), col="red") +
+    geom_line(mapping=aes(x=Fecha, y=Pos_I, group=1), col="blue") +
+    geom_point(mapping=aes(x=Fecha, y=Pos_I, group=1), col="blue") +
+    geom_line(mapping=aes(x=Fecha, y=Pos_L, group=1), col="darkgreen") +
+    geom_point(mapping=aes(x=Fecha, y=Pos_L, group=1), col="darkgreen") +
+    geom_vline(xintercept=which(subtable$Fecha == "2020-03-13"), col="black", lty=2) # day with the first local infected person
+}
+
+dev.off()
+
+pdf("MexicoInfectionToday.pdf", 11, 9)
+ggplot(subtable) +
   ylab("Infected people") + xlab("Date") + ggtitle("Mexico infected people") +
   ylim(c(0,250))+
-  geom_line(subtable, mapping=aes(x=Fecha, y=Pos, group=1)) +
-  geom_point(subtable, mapping=aes(x=Fecha, y=Pos, group=1)) +
+  geom_line(mapping=aes(x=Fecha, y=Pos, group=1)) +
+  geom_point(mapping=aes(x=Fecha, y=Pos, group=1)) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   theme(plot.title = element_text(hjust = 0.5)) +
   geom_hline(yintercept = 100, lty=2) +
-  geom_line(subtable, mapping=aes(x=Fecha, y=Deceased, group=1), col="red") +
-  geom_point(subtable, mapping=aes(x=Fecha, y=Deceased, group=1), col="red") +
-  geom_line(subtable, mapping=aes(x=Fecha, y=Pos_I, group=1), col="blue") +
-  geom_point(subtable, mapping=aes(x=Fecha, y=Pos_I, group=1), col="blue") +
-  geom_line(subtable, mapping=aes(x=Fecha, y=Pos_L, group=1), col="darkgreen") +
-  geom_point(subtable, mapping=aes(x=Fecha, y=Pos_L, group=1), col="darkgreen") +
-  geom_vline(xintercept=which(subtable$Fecha == "2020-03-13"), col="purple", lty=2) # day with the first local infected person
+  geom_line(mapping=aes(x=Fecha, y=Deceased, group=1), col="red") +
+  geom_point(mapping=aes(x=Fecha, y=Deceased, group=1), col="red") +
+  geom_line(mapping=aes(x=Fecha, y=Pos_I, group=1), col="blue") +
+  geom_point(mapping=aes(x=Fecha, y=Pos_I, group=1), col="blue") +
+  geom_line(mapping=aes(x=Fecha, y=Pos_L, group=1), col="darkgreen") +
+  geom_point(mapping=aes(x=Fecha, y=Pos_L, group=1), col="darkgreen") +
+  geom_vline(xintercept=which(subtable$Fecha == "2020-03-13"), col="black", lty=2) # day with the first local infected person
 
 dev.off()
 
